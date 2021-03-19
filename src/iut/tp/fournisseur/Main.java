dcfc0bc13cf3ecbace1c3a2cf511ceead07e5406
Elouan Moreau <e.moreau@compethance.fr>
2021-03-19 08:15:29 +0100

    Glouton

--- src/iut/tp/fournisseur/Main.java
+++ src/iut/tp/fournisseur/Main.java
@@ -5,7 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		
 		FileReader f = new FileReader(args[0]);
-		f.glouton();
+		System.out.println("Eval(i)");
+		System.out.println();
+		System.out.println(f.evalBis());
+		System.out.println();
+		System.out.println("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+		System.out.println();
+		System.out.println("Algorithme glouton : ");
+		System.out.println();
+		f.jenaimarre();
+//		System.out.println(f.eval(1));
 //		System.out.println(f.aff());
 //		System.out.println(f.evalBis());
 //		System.out.println(f.evalBis().size());
